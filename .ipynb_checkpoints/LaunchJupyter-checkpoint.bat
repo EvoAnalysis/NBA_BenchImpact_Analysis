c210cecdee7843bfb4626e1347c5da3ad1176ab9
Evon Williams <Evon41111@hotmail.com>
2025-06-22 03:01:47 -0400

    minor updates

---
+++ .ipynb_checkpoints/LaunchJupyter-checkpoint.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Activate your Anaconda environment (if needed)
+CALL C:\Users\Veteran\anaconda3\Scripts\activate.bat
+
+REM Launch Jupyter Lab in the specific folder
+CALL jupyter lab "C:\Users\Veteran\Desktop\NBA_BenchImpact_Analysis"
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NBA_BenchImpact_Analysis
+-Historical data from 1976–1983 will be added using Basketball Reference as a secondary data source in Phase 2.
